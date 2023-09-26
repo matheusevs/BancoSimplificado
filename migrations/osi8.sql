1738674823ac539c26ef27bd6cf58096825c3e01
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-09-26 13:05:49 -0300

    Alteração na migration de user

--- migrations/osi8.sql
+++ migrations/osi8.sql
@@ -22,11 +22,14 @@ CREATE TABLE IF NOT EXISTS participantes
 CREATE TABLE IF NOT EXISTS users
 (
 
-    id              int unsigned not null primary key auto_increment,
-    name            varchar(255) not null,
-    email           varchar(255) not null,
-    password        varchar(255) not null,
+    id              INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name            VARCHAR(255) NOT NULL,
+    email           VARCHAR(255) NOT NULL,
+    password        VARCHAR(255) NOT NULL,
+    roles           ENUM('admin', 'participant') DEFAULT 'participant' NOT NULL,
     hora_registro   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     hora_update     TIMESTAMP NULL DEFAULT NULL
 
 );
+
+INSERT INTO users (name, email, password, roles) VALUES ('Administrador', 'admin@admin.com.br', sha1('admin'), 'admin');
