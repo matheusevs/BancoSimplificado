774f62e1389c982c74ec6a044b081e79c93daad5
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-09-28 00:49:15 -0300

    Removendo titulo

--- src/views/listarUsuarios.php
+++ src/views/listarUsuarios.php
@@ -19,9 +19,6 @@ $UserController = new UserController();
             <a href="/" class="btn btn-back">
                 <i class="fas fa-arrow-left"></i> Voltar
             </a>
-            <div class="row">
-                <h1 class="display-4 text-center col">EVENTO</h1>
-            </div>
 
             <div class="row justify-content-center">
                 <div class="col-md-10">
